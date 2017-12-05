ac28349fdf0d14d9cf96df7727c15801557c6e91
Mohamed Ziyau <mz_4556@intra.police.gov.mv>
2017-12-05 22:59:28 +0500

    changed some code to es6 syntax to check babel transpiling

--- buildScripts/srcServer.js
+++ buildScripts/srcServer.js
@@ -1,9 +1,9 @@
-var express = require('express');
+import express from 'express';
 var path = require('path');
 var open = require('open');
 
 var port = 3000;
-var app = express();
+const app = express();
 
 app.get('/', function(req, res){
   res.sendFile(path.join(__dirname, '../src/index.html'));
